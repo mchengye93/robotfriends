11b7fb4e89369d328b195d082fe1ce25f8d0af11
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-17 18:10:12 -0700

    Updates

---
+++ precache-manifest.04846003feeb3a62477ff2ccb2d7b9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd50fcf4a11e9b96bbbdfcbe14b0321",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "0bb82c01bb42bf48a6da",
+    "url": "/robotfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "eeb9f730d9acd0db7e0c",
+    "url": "/robotfriends/static/css/main.f1abd05a.chunk.css"
+  },
+  {
+    "revision": "0bb82c01bb42bf48a6da",
+    "url": "/robotfriends/static/js/2.3b08c0f7.chunk.js"
+  },
+  {
+    "revision": "eeb9f730d9acd0db7e0c",
+    "url": "/robotfriends/static/js/main.b86b6b06.chunk.js"
+  },
+  {
+    "revision": "d9953065c9c3306ad6b5",
+    "url": "/robotfriends/static/js/runtime-main.08a6fd18.js"
+  }
+]);
