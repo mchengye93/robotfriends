ad5ede6a186c6b9d7a282950cdf078fc6a48a201
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-18 16:16:08 -0700

    setup store

--- src/reducers.js
+++ src/reducers.js
@@ -5,9 +5,9 @@ const initialState = {
 }
 
 export const searchRobots = (state=initialState, action={}) => {
-    switch(action.type) {
-        case: CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, searchField: action.payload);
+    switch (action.type) {
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, {searchField:action.payload});
         default:
             return state;
     }
