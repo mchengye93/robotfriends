110760ec4f54ba6c13e7ac1bb739d42077c76126
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-18 17:11:04 -0700

    request robots actions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 
@@ -14,7 +15,7 @@ import 'tachyons';
 
 const logger = createLogger();
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(<Provider store={store}>
     <App />
