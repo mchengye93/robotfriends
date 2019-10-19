4c89976880a5f1a763ca3e2525b3a7b356855b67
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-18 17:28:00 -0700

    asynch setup for redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,46 +7,43 @@ import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
 
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
     
 }
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            robots:[],
-
-        }
-        
-    }
-    componentDidMount() {
    
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots:users}));
+    componentDidMount() {
+        this.props.onRequestRobots();
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => this.setState({robots:users}));
 
     }
 
     render() {
-        let {robots} = this.state;
-        const {searchField, onSearchChange} = this.props;
+       
+        const {searchField, onSearchChange, robots , isPending} = this.props;
 
         let filteredRobots = robots.filter((robot) => {
             return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
         })
 
-        if(!robots.length ) {
+        if(isPending) {
             return (
                 <div className='App tc'>
                 <h1>RoboFriends</h1>
